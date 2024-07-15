1435d8040ecae58d734167cd914c95ef40e65745
ksookdhew <ksookdhew@jhb.dvt.co.za>
2024-07-15 10:39:40 +0200

    fix: :mute: removed console logs

--- src/api.ts
+++ src/api.ts
@@ -5,11 +5,6 @@ export async function fetchCityWeather(city: City): Promise<WeatherResponse> {
     `https://api.open-meteo.com/v1/forecast?latitude=${city.latitude}&longitude=${city.longitude}&current=temperature_2m,weather_code`
   );
   const weatherData: WeatherResponse = await response.json();
-
-  console.log(
-    `Current temperature in ${city.name}: ${weatherData.current.temperature_2m}°C`
-  );
-
   return weatherData;
 }
 
@@ -20,13 +15,5 @@ export async function fetchCity7DayForecast(
     `https://api.open-meteo.com/v1/forecast?latitude=${city.latitude}&longitude=${city.longitude}&daily=temperature_2m_max,temperature_2m_min,weather_code`
   );
   const weatherData: DailyWeatherResponse = await response.json();
-
-  console.log(`7-Day Forecast for ${city.name}:`);
-  weatherData.daily.time.forEach((date, index) => {
-    console.log(
-      `Date: ${date}, Max Temp: ${weatherData.daily.temperature_2m_max[index]}°C, Min Temp: ${weatherData.daily.temperature_2m_min[index]}°C, Weather Code: ${weatherData.daily.weather_code[index]}`
-    );
-  });
-
   return weatherData;
 }
